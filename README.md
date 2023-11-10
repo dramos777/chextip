bdb23582d44c38017c6a70ff5f643fd95fb11ec9
Emanuel Dramos <emanuel.dramos2@gmail.com>
2023-11-10 20:34:15 -0300

    Initial code

---
+++ README.md
@@ -0,0 +1,58 @@
+# Change ATA IP Script
+
+## Description
+This Bash script is designed to automate the process of changing the IP address of an ATA (Analog Telephone Adapter) device. It reads from an inventory file informations about the infra enviroment and modifies the necessary configurations to update the ATA IP. The script utilizes various functions and environment variables to execute the necessary actions.
+
+## Instructions
+To use this script, follow the steps below:
+
+1. Make sure you have the necessary environment variables properly configured.
+2. Ensure that the required files and directories are in place.
+4. Run the script with appropriate arguments.
+5. Make sure RouterOS IP and phone extension is configured.
+6. Make sure client-dhcp is configured in ATA
+7. Make sure RouterOS leases is configured with ATA IP and properly comment.
+8. Make sure you have the following dependencies installed:
+
+- Python
+- Selenium
+- WebDriver (the script uses the Firefox browser)
+- Telnet
+- Sshpass
+
+## Script Overview
+
+- The script sets up necessary variables and checks for the presence of required utilities like `sshpass` and `telnet`.
+- It imports functions from the main file and environment variables from the environment file.
+- The script updates the environment variables and performs the necessary tasks to change the ATA IP address.
+- It handles potential errors and logs the events in the designated log directory.
+
+## Prerequisites
+- Bash shell
+- `sshpass` and `telnet` should be installed.
+- Ensure the presence of the necessary configuration files and directories.
+
+## Compatibility
+The script is compatible with GNU/Linux environments. It has been tested on the following ATA devices:
+
+- Grandstream (models: HT-503)
+- Linksys (models: SPA 3000)
+- Intelbras (models: ATA 200)
+
+## Usage
+Run the script with the appropriate arguments, as demonstrated below:
+
+```bash
+./chextip <phone_extension_number> [model]
+```
+
+## History
+
+v1.0 08/11/2023, Emanuel Dramos:
+- Initial code
+- README.md
+- Push to github
+
+### Maintainer
+Emanuel Dramos
+- **Github:** https://github.com/dramos777
---
+++ selenium/main.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+
+
+### Interbras/ATA200
+def intelbras_ata200():
+    firefox = webdriver.Firefox()
+    firefox.get("URL")
+
+    # Wait for
+    WebDriverWait(firefox, 10).until(
+    EC.visibility_of_element_located(("name", "username"))
+    )
+
+    # Find the user field
+    username = firefox.find_element("name", "username")
+    username.send_keys("HTTP_USER")
+
+    # Find the password field
+    password = firefox.find_element("name", "password")
+    password.send_keys("HTTP_PASSWORD")
+
+    # Find the login button and click it
+    login_button = firefox.find_element("id", "login_button")
+    login_button.click()
+
+    time.sleep(3)
+
+    # Switch to menuFrame and open it (tools and reboot menu)
+    firefox.switch_to.frame("menuFrame")
+
+    # Wait for
+    WebDriverWait(firefox, 10).until(
+    EC.visibility_of_element_located(("id", "strTools"))
+    )
+    tools_menu = firefox.find_element("id", "strTools")
+    tools_menu.click()
+    reboot_menu = firefox.find_element("id", "strDeviceRestart")
+    reboot_menu.click()
+
+    # Switch to default frame
+    firefox.switch_to.default_content()
+
+    time.sleep(3)
+
+    # Switch to mainframe and reboot device
+    firefox.switch_to.frame("mainframe")
+
+    # Wait for
+    WebDriverWait(firefox, 10).until(
+    EC.visibility_of_element_located(("id", "strReboot"))
+    )
+    reboot_button = firefox.find_element("id", "strReboot")
+    reboot_button.click()
+
+    # PopUP - Wait and accept
+    time.sleep(7)
+    alert = firefox.switch_to.alert
+    alert.accept()
+
+    firefox.quit()
+
+### Khomp
+def khomp():
+    firefox = webdriver.Firefox()
+    firefox.get("URL")
+
+    # Wait for
+    WebDriverWait(firefox, 10).until(
+    EC.visibility_of_element_located(("name", "pass"))
+    )
+
+    # Find the password field
+    password = firefox.find_element("name", "pass")
+    password.send_keys("admin")
+    password.send_keys(Keys.ENTER)
+
+    time.sleep(3)
+
+    # Switch to menuFrame and open it (tools and reboot menu)
+    firefox.switch_to.default_content()
+    firefox.switch_to.frame("menuFrame")
+
+
+    restart_button = firefox.find_element(By.XPATH, '//input[@value="REINICIAR" and @style="width:100%"]')
+    time.sleep(7)
+    restart_button.click()
+
+
+    # PopUP - Wait and accept
+    time.sleep(7)
+    alert = firefox.switch_to.alert
+    alert.accept()
+
+    firefox.quit()
+
+PYTHON_COMMAND()
+
