f8c4557aeff309f231905c6a265b25557b236eb1
dramos <emanuel.dramos2@gmail.com>
2024-10-22 14:56:41 -0300

    Added gunicorn logs to stdout

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gunicorn --chdir /app app:app --bind 0.0.0.0:5000 --workers 3
+gunicorn --access-logfile - --error-logfile - --chdir /app app:app --bind 0.0.0.0:5000 --workers 3
