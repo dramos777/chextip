8e040043d2e884b8a28887b87beb8f0486f258fa
dramos <emanuel.dramos2@gmail.com>
2024-10-30 17:18:38 -0300

    Up worker number to 5

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gunicorn --timeout 120 --access-logfile - --error-logfile - --chdir /app app:app --bind 0.0.0.0:5000 --workers 3
+gunicorn --timeout 120 --access-logfile - --error-logfile - --chdir /app app:app --bind 0.0.0.0:5000 --workers 5
